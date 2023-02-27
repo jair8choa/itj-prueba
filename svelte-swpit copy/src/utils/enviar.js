5c0272a0ab03a8870dc0dd2f752f5a92ba89e44b
Joaquin Jairo Ochoa Canela <43918449+jair8choa@users.noreply.github.com>
2023-02-27 09:47:20 -0600

    Add files via upload

---
+++ svelte-swpit copy/src/utils/enviar.js
@@ -0,0 +1,39 @@
+import { Router, Link, Route, navigate } from "svelte-routing";
+
+const enviar = (respuestas, token, id_encuesta) => {
+  respuestas = respuestas.map((element) => {
+    return element.filter((i) => i != "" && i != null);
+  });
+
+  const body = {
+    respuestas,
+    id_encuesta,
+  };
+
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: token,
+      Accept: "*/*",
+    },
+    body: JSON.stringify(body),
+  };
+
+  // fetch("https://swpit-jwt-test-7cazqrq4mq-uc.a.run.app/encuesta/resultados", options)
+  fetch("http://127.0.0.1:5050/encuesta/resultados", options)
+    .then((response) => response.text())
+    .then((data) => {
+      navigate("/resultados"+id_encuesta, { replace: true });
+    })
+    .catch((error) => {
+      navigate("/encuestas", { replace: true });
+    });
+};
+
+const terminarSesion = ()=>{
+  alert("Tu sesion ha terminado, inicia sesion de nuevo")
+  localStorage.clear()
+  navigate('/', {replace: true})
+}
+
+export { enviar, terminarSesion};
